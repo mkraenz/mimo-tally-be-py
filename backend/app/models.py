f5f29ae86a09b0ecb4db2623f2b4cbef0473d3c9
Mirco Kraenz <software@kraenz.eu>
2025-06-02 07:38:38 +0200

    refactor: MIGRATION rm item table

--- backend/app/models.py
+++ backend/app/models.py
@@ -30,7 +30,6 @@ class UserBase(SQLModel):
 class User(UserBase, table=True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     clerk_user_id: str | None = Field(None, unique=True, index=True)
-    items: list["Item"] = Relationship(back_populates="owner", cascade_delete=True)
     owned_settlements: list["Settlement"] = Relationship(
         back_populates="owner",
         cascade_delete=True,
@@ -56,22 +55,6 @@ class User(UserBase, table=True):
 # Properties to return via API, id is always required
 class UserPublic(UserBase):
     id: uuid.UUID
-
-
-# Shared properties
-class ItemBase(SQLModel):
-    title: str = Field(min_length=1, max_length=255)
-    description: str | None = Field(default=None, max_length=255)
-
-
-# TODO rm
-# Database model, database table inferred from class name
-class Item(ItemBase, table=True):
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
-    owner_id: uuid.UUID = Field(
-        foreign_key="user.id", nullable=False, ondelete="CASCADE"
-    )
-    owner: User | None = Relationship(back_populates="items")
 
 
 class Currency(Enum):
